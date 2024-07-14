daf0bc2877c331ed88f270532fd2661d788132f5
kuruhakr <157008277+kuruhakr@users.noreply.github.com>
2024-07-14 12:55:34 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -35,7 +35,7 @@ const CONFIG = {
 
   // notion configuration (required)
   notionConfig: {
-    pageId: process.env.NOTION_PAGE_ID || "", // Set your Notion page ID here or retrieve from environment variables
+    pageId: process.env.NOTION_PAGE_ID,
   },
 
   // plugin configuration (optional)
@@ -51,10 +51,16 @@ const CONFIG = {
       siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
     },
   },
+  naverSearchAdvisor: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION || "",
+    },
+  },
   utterances: {
     enable: true,
     config: {
-      repo: "kuruhakr/blog_1",
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
@@ -68,6 +74,6 @@ const CONFIG = {
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
   revalidateTime: 21600 * 7, // revalidate time for [slug], index
-};
+}
 
 module.exports = { CONFIG }
